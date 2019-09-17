aa2c4e773b86c8a98d1238e0168dfc5b4eaa32b8
Mirza Chilman <mirza@accelbyte.net>
2019-09-17 17:43:00 +0700

    finish fetch nearby places

--- src/App.js
+++ src/App.js
@@ -1,60 +1,168 @@
 import React, { Component } from "react";
-import { render } from "react-dom";
-import Map from "./components/Map";
-import InfoWindow from "./components/InfoWindow";
+import localRestaurant from "./localRestaurant";
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react";
+import RestaurantCard from "./RestaurantCard";
+import Axios from "axios";
+import "./App.css";
+
+const apiKey = "AIzaSyDFjB3G4gf-SuD7ho1tYDj8nBwCzoDR22I";
 
 class App extends Component {
   state = {
+    initialLocation: {
+      lat: 0,
+      lng: 0,
+    },
     locations: [
-      ["Bondi Beach", -33.890542, 151.274856, 4],
-      ["Coogee Beach", -33.923036, 151.259052, 5],
-      ["Cronulla Beach", -34.028249, 151.157507, 3],
-      ["Manly Beach", -33.80010128657071, 151.28747820854187, 2],
-      ["Maroubra Beach", -33.950198, 151.259302, 1],
+      { lat: -33.890542, long: 151.274856, zoom: 4, name: "Bondi" },
+      { lat: -33.790542, long: 151.274856, zoom: 4, name: "Necha" },
     ],
+    googleLocations: [],
   };
-  createInfoWindow = (e, map) => {
-    const infoWindow = new window.google.maps.InfoWindow({
-      content: '<div id="infoWindow" />',
-      position: { lat: e.latLng.lat(), lng: e.latLng.lng() },
-    });
-    infoWindow.addListener("domready", e => {
-      render(<InfoWindow />, document.getElementById("infoWindow"));
-    });
-    infoWindow.open(map);
+  static defaultProps = {
+    center: {
+      lat: -7.7833528,
+      lng: 110.4343039,
+    },
+    zoom: 14,
   };
 
-  createMarkers = map => {
-    const { locations } = this.state;
-    locations.map((location, i) => {
-      const marker = new window.google.maps.Marker({
-        position: new window.google.maps.LatLng(
-          locations[i][1],
-          locations[i][2]
-        ),
-        map: map,
-      });
+  myMapRef = React.createRef();
 
-      marker.addListener("click", marker => {
-        this.createInfoWindow(marker, map);
-      });
+  getInitialLocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        position => {
+          this.setState({
+            initialLocation: {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude,
+            },
+          });
+        },
+        error => console.log(error)
+      );
+    } else {
+      console.log("something wrong");
+    }
+  };
+
+  async componentDidMount() {
+    await this.getInitialLocation();
+  }
+
+  callback = (result, status) => {
+    this.setState({
+      googleLocations: result,
     });
+    console.log(result);
+    console.log(status);
+  };
+
+  fetchPlaces = async (mapProps, map) => {
+    const { google } = mapProps;
+    const service = new google.maps.places.PlacesService(map);
+    this.setState({});
+    const mapLocation = map.center;
+    const request = {
+      location: mapLocation,
+      radius: 1000,
+      types: ["restaurant"],
+    };
+    return await service.nearbySearch(request, this.callback);
+  };
+
+  mapClicked(mapProps, map, clickEvent) {
+    // ...
+    console.log(clickEvent);
+    console.log(mapProps);
+    console.log(map);
+  }
+
+  onMarkerClick(mapProps, marker, e) {
+    // ..
+    console.log(mapProps);
+    console.log(marker);
+    console.log(e);
+  }
+
+  onMouseoverMarker(mapProps, marker, e) {
+    // ..
+    console.log(mapProps);
+    console.log(marker);
+    console.log(e);
+  }
+
+  renderLoading = () => <h1>Loading</h1>;
+
+  renderMap = () => {
+    const { googleLocations } = this.state;
+    return (
+      <Map
+        google={this.props.google}
+        name={"Current Location"}
+        initialCenter={{
+          lat: this.state.initialLocation.lat,
+          lng: this.state.initialLocation.lng,
+        }}
+        ref={this.myMapRef}
+        style={{ height: "100vh", width: "100%" }}
+        onReady={this.fetchPlaces}
+        onClick={this.mapClicked}
+      >
+        {localRestaurant.map(location => {
+          return (
+            <Marker
+              title={location.restaurantName}
+              name={location.restaurantName}
+              position={{ lat: location.lat, lng: location.long }}
+              onClick={this.onMarkerClick}
+              onMouseover={this.onMouseoverMarker}
+            />
+          );
+        })}
+        {googleLocations &&
+          googleLocations.length > 0 &&
+          googleLocations.map(location => {
+            const lat = location.geometry.location.lat();
+            const lng = location.geometry.location.lng();
+            return (
+              <Marker
+                title={location.name}
+                name={location.name}
+                position={{ lat, lng }}
+                onClick={this.onMarkerClick}
+                onMouseover={this.onMouseoverMarker}
+              />
+            );
+          })}
+      </Map>
+    );
+  };
+
+  renderMenuRightBar = () => {
+    return (
+      <div className={"something"}>
+        {localRestaurant.map(restaurant => {
+          return <RestaurantCard {...restaurant} />;
+        })}
+      </div>
+    );
   };
 
   render() {
+    console.log(this.myMapRef);
+    const { lat, lng } = this.state.initialLocation;
+    const initialLocationHasNotLoaded = lat === 0 && lng === 0;
     return (
-      <>
-        <Map
-          id="myMap"
-          options={{
-            center: { lat: -33.890542, lng: 151.274856 },
-            zoom: 8,
-          }}
-          onMapLoad={map => this.createMarkers(map)}
-        />
-      </>
+      <div className={"mapContainer"}>
+        {initialLocationHasNotLoaded ? this.renderLoading() : this.renderMap()}
+        {this.renderMenuRightBar()}
+      </div>
     );
   }
 }
 
-export default App;
+export default GoogleApiWrapper({
+  apiKey: apiKey,
+})(App);
